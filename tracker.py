cf10d8986f6349c21a314a5a5eeedfffd3f5ba1e
vanluwin <richardsonsantiago@ufrn.edu.br>
2020-04-17 09:49:00 -0300

    Encapsulating common functions into utils.py

--- tracker.py
+++ tracker.py
@@ -1,4 +1,4 @@
-from math import atan2, cos, sin, sqrt, pi
+from utils import drawAxis, getOrientation
 import numpy as np
 import cv2 as cv
 import argparse
@@ -44,63 +44,6 @@ def parser_args():
     )
 
     return parser.parse_args()
-
-def drawAxis(img, p_, q_, colour, scale):
-    p = list(p_)
-    q = list(q_)
-
-    angle = atan2(p[1] - q[1], p[0] - q[0])  # angle in radians
-    hypotenuse = sqrt((p[1] - q[1]) * (p[1] - q[1]) + (p[0] - q[0]) * (p[0] - q[0]))
-
-    # Here we lengthen the arrow by a factor of scale
-    q[0] = p[0] - scale * hypotenuse * cos(angle)
-    q[1] = p[1] - scale * hypotenuse * sin(angle)
-    cv.line(img, (int(p[0]), int(p[1])), (int(q[0]), int(q[1])), colour, 2, cv.LINE_AA)
-
-    # create the arrow hooks
-    p[0] = q[0] + 9 * cos(angle + pi / 4)
-    p[1] = q[1] + 9 * sin(angle + pi / 4)
-    cv.line(img, (int(p[0]), int(p[1])), (int(q[0]), int(q[1])), colour, 2, cv.LINE_AA)
-
-    p[0] = q[0] + 9 * cos(angle - pi / 4)
-    p[1] = q[1] + 9 * sin(angle - pi / 4)
-    cv.line(img, (int(p[0]), int(p[1])), (int(q[0]), int(q[1])), colour, 2, cv.LINE_AA)
-
-def getOrientation(pts, img, draw):
-    # Construct a buffer used by the pca analysis
-    sz = len(pts)
-    data_pts = np.empty((sz, 2), dtype=np.float64)
-
-    for i in range(data_pts.shape[0]):
-        data_pts[i, 0] = pts[i, 0, 0]
-        data_pts[i, 1] = pts[i, 0, 1]
-
-    # Perform PCA analysis
-    mean = np.empty((0))
-    mean, eigenvectors, eigenvalues = cv.PCACompute2(data_pts, mean)
-
-    # Store the center of the object
-    cntr = (int(mean[0, 0]), int(mean[0, 1]))
-
-    # Draw the principal components
-    cv.circle(img, cntr, 3, (42, 89, 247), -1)
-    p1 = (
-        cntr[0] + 0.02 * eigenvectors[0, 0] * eigenvalues[0, 0],
-        cntr[1] + 0.02 * eigenvectors[0, 1] * eigenvalues[0, 0]
-    )
-    p2 = (
-        cntr[0] - 0.02 * eigenvectors[1,0] * eigenvalues[1,0],
-        cntr[1] - 0.02 * eigenvectors[1,1] * eigenvalues[1,0]
-    )
-
-    if(draw):
-        drawAxis(img, cntr, p1, (91, 249, 77), 2)
-        drawAxis(img, cntr, p2, (190, 192, 91), 2)
-
-    # orientation in radians
-    angle = atan2(eigenvectors[0, 1], eigenvectors[0, 0])
-
-    return cntr, angle
 
 if __name__ == '__main__':
     args = parser_args()
@@ -222,10 +165,11 @@ if __name__ == '__main__':
             # Find the orientation of each shape
             current_pos, _ = getOrientation(c, frame, args.draw_axis)
 
-        speed = sqrt(
+        speed = np.sqrt(
             (previous_pos[0] - current_pos[0])**2 + 
             (previous_pos[1] - current_pos[1])**2
         )
+
         traveledDistance += speed
         previous_pos = current_pos
 
